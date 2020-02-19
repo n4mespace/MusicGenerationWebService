0cf14648441b1d115903e7a54340d12eb1783a28
n4mespace <zaxarchuk.d@gmail.com>
2020-02-19 06:07:49 +0200

    [add]:  service root

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from quart import jsonify, request
+from quart import jsonify, request, render_template
 from app.main import bp
 from quart_openapi import Resource
 
@@ -13,3 +13,10 @@ class HealthCheck(Resource):
         ''' Testing if app posts correctly '''
         data = await request.get_data()
         return jsonify({'status': 'OK'})
+
+
+@bp.route('/service', methods=['GET'])
+class Service(Resource):
+    async def get(self):
+        ''' Main page '''
+        return await render_template('index.html')
